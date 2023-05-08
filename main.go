05a9101e9857e99171ef4a4fc6794e9358e1653b
go_khopipah-parawansah-siregar <khopipahparawansah@gmail.com>
2023-05-08 18:16:05 +0700

    main.go

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
-	"khopipah_mini_project/config"
-	"khopipah_mini_project/routes"
+	"khopipah_mini_project-1/config"
+	//m "khopipah_mini_project_structure/middlewares"
+	"khopipah_mini_project-1/routes"
 )
 
 func main() {
 	config.InitDB()
 	e := routes.New()
+	//m.LogMiddlewares(e)
+
 	e.Logger.Fatal(e.Start(":8000"))
 }
