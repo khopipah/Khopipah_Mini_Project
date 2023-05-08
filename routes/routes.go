2d895fd78fcb6c3c3a2c70941da1178874e91084
go_khopipah-parawansah-siregar <khopipahparawansah@gmail.com>
2023-05-08 18:15:34 +0700

    routes docter/pasien

--- routes/routes.go
+++ routes/routes.go
@@ -1,20 +1,49 @@
 package routes
 
 import (
-	"khopipah_mini_project/controllers"
+	"khopipah_mini_project-1/controllers"
+	"net/http"
 
-	"github.com/labstack/echo"
+	"github.com/go-playground/validator"
+	"github.com/labstack/echo/v4"
 )
+
+type CustomValidator struct {
+	validator *validator.Validate
+}
+
+func (cv *CustomValidator) Validate(i interface{}) error {
+	if err := cv.validator.Struct(i); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+	return nil
+}
 
 func New() *echo.Echo {
 	e := echo.New()
+	//route docter
+	docter := e.Group("/docters")
+	docter.GET("", controllers.GetDocterControllers)
+	docter.POST("", controllers.CreateDocterController)
+	docter.GET(":id", controllers.GetDocterController)
+	docter.PUT(":id", controllers.UpdateDocterController)
+	docter.DELETE(":id", controllers.DeleteDocterController)
 
-	//routes user
-	e.GET("/users", controllers.GetUserControllers)
-	e.POST("/users", controllers.CreateUserController)
-	e.GET("/users/:id", controllers.GetUserController)
-	e.PUT("/users/:id", controllers.UpdateUserController)
-	e.DELETE("/users/:id", controllers.DeleteUserController)
+	//route pasien
+	pasien := e.Group("/pasiens")
+	pasien.GET("", controllers.GetPasienControllers)
+	pasien.POST("", controllers.CreatePasienController)
+	pasien.GET(":id", controllers.GetPasienController)
+	pasien.PUT(":id", controllers.UpdatePasienController)
+	pasien.DELETE(":id", controllers.DeletePasienController)
+
+	//route pasien
+	room := e.Group("/rooms")
+	room.GET("", controllers.GetRoomControllers)
+	room.POST("", controllers.CreateRoomController)
+	room.GET(":id", controllers.GetRoomController)
+	room.PUT(":id", controllers.UpdateRoomController)
+	room.DELETE(":id", controllers.DeleteRoomController)
 
 	return e
 }
