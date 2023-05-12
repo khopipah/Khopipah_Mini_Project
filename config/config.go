ef4a8266a6809e785ee4e5b7cb807381804fe076
go_khopipah-parawansah-siregar <khopipahparawansah@gmail.com>
2023-05-12 15:58:48 +0700

    Add File Config

--- config/config.go
+++ config/config.go
@@ -2,7 +2,7 @@ package config
 
 import (
 	"fmt"
-	"khopipah_mini_project-1/models"
+	"khopipah_mini_project/model"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -10,43 +10,31 @@ import (
 
 var DB *gorm.DB
 
-func init() {
-	InitDB()
-	InitMigrate()
-}
-
-func InitDB() {
-
-	config := struct {
-		DB_Username string
-		DB_Password string
-		DB_Port     string
-		DB_Host     string
-		DB_Name     string
-	}{
-		DB_Username: "root",
-		DB_Password: "",
-		DB_Host:     "localhost",
-		DB_Port:     "3306",
-		DB_Name:     "mini_project_structure",
+func InitDB() *gorm.DB {
+	config := map[string]string{
+		"DB_Username": "root",
+		"DB_Password": "",
+		"DB_Port":     "3306",
+		"DB_Host":     "localhost",
+		"DB_Name":     "golang_mini_project",
 	}
 
 	connectionString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
-		config.DB_Username,
-		config.DB_Password,
-		config.DB_Host,
-		config.DB_Port,
-		config.DB_Name,
-	)
+		config["DB_Username"],
+		config["DB_Password"],
+		config["DB_Host"],
+		config["DB_Port"],
+		config["DB_Name"])
 
-	var err error
-	DB, err = gorm.Open(mysql.Open(connectionString), &gorm.Config{})
-	if err != nil {
-		panic(err)
+	var e error
+	DB, e = gorm.Open(mysql.Open(connectionString), &gorm.Config{})
+	if e != nil {
+		panic(e)
 	}
 	InitMigrate()
+	return DB
 }
 
 func InitMigrate() {
-	DB.AutoMigrate(&models.User{}, &models.Docter{}, &models.Pasien{}, &models.Room{})
+	DB.AutoMigrate(&model.User{}, &model.Docter{}, &model.Pasien{}, &model.Room{})
 }
